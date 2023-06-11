ad2d9dab20581578aee22ba079f6def803c9372b
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-06-11 17:43:52 +0300

    innit

--- src/main/java/HttpImageStatusCli.java
+++ src/main/java/HttpImageStatusCli.java
@@ -6,26 +6,26 @@ public class HttpImageStatusCli {
     public void askStatus(){
         HttpStatusImageDownloader httpStatusImageDownloader=new HttpStatusImageDownloader();
         String statusCode;
-        Scanner scanner=new Scanner(System.in);
-        String regex = "[0-9]+[\\.]?[0-9]*";
+        try (Scanner scanner = new Scanner(System.in)) {
+            String regex = "[0-9]+[\\.]?[0-9]*";
 
-        while (true){
-            try {
-                System.out.println("Enter HTTP status code: ");
-                statusCode=scanner.nextLine();
-                if(Pattern.matches(regex,statusCode)){
-                    httpStatusImageDownloader.downloadStatusImage(Integer.parseInt(statusCode));
-                    System.out.println("Image downloaded.");
-                    break;
-                }else {
-                    System.out.println("Invalid HTTP status code.");
+            while (true) {
+                try {
+                    System.out.println("Enter HTTP status code: ");
+                    statusCode = scanner.nextLine();
+                    if (Pattern.matches(regex, statusCode)) {
+                        httpStatusImageDownloader.downloadStatusImage(Integer.parseInt(statusCode));
+                        System.out.println("Image downloaded.");
+                        break;
+                    } else {
+                        System.out.println("Invalid HTTP status code.");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Please enter a valid number.");
+                    scanner.nextLine();
                 }
-            } catch (Exception e) {
-                System.out.println("Please enter a valid number.");
-                scanner.nextLine();
             }
         }
-        scanner.close();
     }
 
 //    public static void main(String[] args) {
